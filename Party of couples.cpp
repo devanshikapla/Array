e997bedabe6c78c9e4f1438cb793df39063903f7
Devanshi Kapla <89089904+devanshikapla@users.noreply.github.com>
2023-06-24 18:50:40 +0530

    Create Party of couples.cpp

---
+++ Party of couples.cpp
@@ -0,0 +1,11 @@
+int findSingle(int N, int arr[]){
+        // code here
+        unordered_map < int , int > mpp;
+        for(int i = 0 ; i < N ; i++){
+            mpp[arr[i]]++;
+        }
+        for(auto it : mpp){
+            if(it.second == 1) return it.first;
+        }
+        return -1;
+}
