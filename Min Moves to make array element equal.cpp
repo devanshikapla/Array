ab1f669cdce4ae076e79164c77cf506e61a0f3c0
Devanshi Kapla <89089904+devanshikapla@users.noreply.github.com>
2023-10-01 13:32:58 +0530

    Create Min Moves to make array element equal.cpp

---
+++ Min Moves to make array element equal.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minMoves(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin() , nums.end());
+        int res = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            res = res + abs(nums[i] - nums[0]);
+        }
+        return res;
+    }
+};
