cc46fb69852ef8a4c8ba65c6dd5b5be736040a20
Devanshi Kapla <89089904+devanshikapla@users.noreply.github.com>
2023-10-28 13:07:44 +0530

    Create Check set bits.cpp

---
+++ Bit Manipulation/Check set bits.cpp
@@ -0,0 +1,14 @@
+class Solution{
+public:
+    int isBitSet(int N){
+        // code here 
+        if(N == 0) return 0;
+        while(N != 0){
+            if((N & 1 ) == 0){
+                return 0;
+            }
+            N >>= 1;
+        }
+        return 1;
+    }
+};
